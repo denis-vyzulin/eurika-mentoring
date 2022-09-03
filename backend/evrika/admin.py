cc8208a87c9d902e4d9e66a3204183136fada6c9
dintear <dintear@yandex.ru>
2022-09-03 22:57:50 +0300

    feat: Describe list of table entries (#1)

--- backend/evrika/admin.py
+++ backend/evrika/admin.py
@@ -3,6 +3,21 @@ from django.contrib import admin
 from .models import User, Mentor, Student
 
 
-admin.site.register(User)
-admin.site.register(Mentor)
-admin.site.register(Student)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'email',
+        'username',
+        'surname',
+        'is_active',
+        'is_staff',
+    ]
+    list_filter = [
+        'is_active',
+        'is_staff'
+    ]
+    ordering = ['-is_active', '-date_joined', 'username']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Mentor, UserAdmin)
+admin.site.register(Student, UserAdmin)
